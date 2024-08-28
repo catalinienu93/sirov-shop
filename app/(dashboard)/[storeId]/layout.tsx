c46096f35978aa35c258f45170c3714f7201a50b
unknown <ienulesccatalin@gmail.com>
2024-08-28 17:22:24 +0300

    Add navigation bar

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,6 +1,7 @@
 import prismadb from '@/lib/prismadb';
 import { auth } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
+import NavBar from '@/components/navbar';
 
 export default async function DashboardLayout({
   children,
@@ -28,7 +29,7 @@ export default async function DashboardLayout({
 
   return (
     <>
-      <div>This will be a Navbar</div>
+      <NavBar />
       {children}
     </>
   );
